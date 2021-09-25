6bab6b0a49072d708223a2c61c279efd99a92db1
Yecid Leyes <yecidleyesb0321@gmail.com>
2021-09-24 21:56:21 -0500

    Se agrega la validacion de nombre

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,5 +1,23 @@
 function validar_nombre_usuario(){
+    
+    const nombre = document.getElementById('nombre_usuario').value;
 
+
+    var exp = /^[A-Z][a-z]+$/;
+    
+    if (nombre == null || nombre.length == 0){
+        alert('El nombre no puede estar vacio\nDebe de empezar con una letra en mayuscula');
+    
+    }
+    else if(nombre.length>30){
+        alert('El nombre es muy largo\nDebe de tener máximo 10 letras');
+    
+    }
+    else if(exp.test(nombre)==false){
+        alert('El nombre no es valido\nDebe de empezar por una letra en mayuscula');
+    
+    }
+    
 }
 
 function validar_anoNacimiento_usuario(){
@@ -16,6 +34,6 @@ function validar_contrasena(){
     }
 }
 
-module.exports.validar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_contrasena = validar_contrasena;
-module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
+// module.exports.validar_nombre_usuario = validar_nombre_usuario;
+// module.exports.validar_contrasena = validar_contrasena;
+// module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
