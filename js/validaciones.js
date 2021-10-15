cd5984fadf930da143885c804e4393f0350c920b
Sebastian Gamboa <sebastiangb007@gmail.com>
2021-10-14 22:15:50 -0500

    sprint 3

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,5 +1,4 @@
 var registros = [];
-//var arreglo=[];
 var person;
 
 function validar_nombre_usuario(String) {
@@ -51,13 +50,12 @@ function agregarRegistro() {
     registros.push(person);
     console.log(registros.toString())
 
-    document.getElementById('nombre_usuario').value = "";
-    document.getElementById('anoNacimiento_usuario').value = "";
-    document.getElementById('contrasena_usuario').value = "";
+    document.getElementById('nombre_usuario').value="";
+    document.getElementById('anoNacimiento_usuario').value="";
+    document.getElementById('contrasena_usuario').value="";
     return registros;
 }
 
-<<<<<<< HEAD
 function EncontrarUsuarioPorEdad(registros) {
   
     var puntero=0
@@ -75,26 +73,8 @@ function EncontrarUsuarioPorEdad(registros) {
     }
     console.log(registros[puntero]);
     return(registros[puntero]);
-=======
-
-function EncontrarUsuarioPorEdad(arreglo) {
-
-    //var arreglo = arreglo || [];
-    arreglo = agregarRegistro();
-    //console.log(arreglo[1][2]);
-
-    var fechaMayor = 0;
-    var usuario = [];
-    for (var i = 0; i < arreglo.length; i++) {
-        if (fechaMayor <= parseInt(arreglo[i][1])) {
-            fechaMayor = parseInt(arreglo[i][1]);
-            usuario = arreglo[i];
-        }
-    }
-    console.log(usuario);
-    return usuario;
->>>>>>> a43d14f5074fea5f492534f225a2dc9bf47f7dbf
 }
+
 
 module.exports.registros = registros;
 module.exports.EncontrarUsuarioPorEdad = EncontrarUsuarioPorEdad;
